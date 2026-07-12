ef600b34cce0aee05ae97fb5866b62133d35dd3f
Martin <mrkacik1@gmail.com>
2026-07-12 22:58:39 +0200

    odkazy

---
+++ googletag.js
@@ -0,0 +1,7 @@
+// Google Analytics - lokální spouštěč knihovny
+(function() {
+    window.dataLayer = window.dataLayer || [];
+    window.gtag = window.gtag || function() { dataLayer.push(arguments); };
+    gtag('js', new Date());
+    gtag('config', 'G-2BW708HYKH');
+})();
