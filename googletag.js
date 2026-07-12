f872cd456297926584bf3cdbf92e86dbbc8276b3
Martin <mrkacik1@gmail.com>
2026-07-12 23:42:53 +0200

    odstranění 2 soborů

--- googletag.js
+++
@@ -1,7 +0,0 @@
-// Google Analytics - lokální spouštěč knihovny
-(function() {
-    window.dataLayer = window.dataLayer || [];
-    window.gtag = window.gtag || function() { dataLayer.push(arguments); };
-    gtag('js', new Date());
-    gtag('config', 'G-2BW708HYKH');
-})();
